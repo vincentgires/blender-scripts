465e235e9b7678c7a37930fdd67666f3d40fcee6
Vincent Girès <vincent.gires@gmail.com>
2016-11-24 18:45:53 +0100

    add setLevels from logging module

--- addons/debug.py
+++ addons/debug.py
@@ -11,11 +11,14 @@ bl_info = {
     "category": "Sequencer"}
 
 
-import bpy, sys
+import bpy, sys, logging
 
 #sys.path.append("C:/SOFT/eclipse/java-mars/plugins/org.python.pydev_4.4.0.201510052309/pysrc/")
 sys.path.append("/home/vincent/.p2/pool/plugins/org.python.pydev_5.3.1.201610311318/pysrc/")
-import pydevd
+try:
+    import pydevd
+except:
+    pass
 
 '''
 class SEQUENCER_debug(bpy.types.Panel):
@@ -29,9 +32,10 @@ class SEQUENCER_debug(bpy.types.Panel):
         layout.operator("debug_disconnect_to_eclipse.btn")
 '''
 
-def draw_eclipse_panel(layout):
+def draw_debug_panel(layout):
     layout.operator("debug_connect_to_eclipse.btn")
     layout.operator("debug_disconnect_to_eclipse.btn")
+    layout.operator_menu_enum("debug_logging_setlevels.menu", "typeEnum")
 
 class VIEW3D_debug(bpy.types.Panel):
     bl_label = "Debug"
@@ -41,7 +45,7 @@ class VIEW3D_debug(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        draw_eclipse_panel(layout)
+        draw_debug_panel(layout)
         
 class NODE_EDITOR_debug(bpy.types.Panel):
     bl_label = "Debug"
@@ -51,7 +55,7 @@ class NODE_EDITOR_debug(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        draw_eclipse_panel(layout)
+        draw_debug_panel(layout)
 
 
 class SEQUENCER_debug(bpy.types.Panel):
@@ -61,7 +65,7 @@ class SEQUENCER_debug(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        draw_eclipse_panel(layout)
+        draw_debug_panel(layout)
 
 
 class DEBUG_connect_to_eclipse(bpy.types.Operator):
@@ -83,13 +87,39 @@ class DEBUG_disconnect_to_eclipse(bpy.types.Operator):
         return{'FINISHED'}
 
         
+
+class DEBUG_logging_setLevels(bpy.types.Operator):
+    bl_idname = "debug_logging_setlevels.menu"
+    bl_label = "Set Logger levels"
+    
+    def get_enum(self, context):
+        return bpy.props.logging_setLevels
+    
+    typeEnum = bpy.props.EnumProperty(
+        items = get_enum
+    )
+    
+    def execute(self, context):
+        logger = logging.getLogger()
+        logger.setLevel(self.typeEnum)
         
+        return {"FINISHED"}
+    
         
 def register():
     bpy.utils.register_module(__name__)
+    bpy.props.logging_setLevels = [
+        ("DEBUG","DEBUG","DEBUG"),
+        ("INFO","INFO","INFO"),
+        ("WARNING","WARNING","WARNING"),
+        ("ERROR","ERROR","ERROR"),
+        ("CRITICAL","CRITICAL","CRITICAL")
+        
+    ]
     
 def unregister():
     bpy.utils.unregister_module(__name__)
+    del bpy.props.logging_setLevels
 
 if __name__ == "__main__":
 	register()
