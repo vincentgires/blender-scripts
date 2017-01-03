3b0a27671f7e4d40384da1e592baac9bf6f3ad6c
Vincent Girès <mail@vincentgires.com>
2017-01-03 02:00:40 +0100

    adding Qt integration example

---
+++ addons/qt_integration/__init__.py
@@ -0,0 +1,105 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
+bl_info = {
+    "name": "Qt Integration",
+    "author": "Vincent Gires",
+    "description": "Qt Integration Example",
+    "version": (0, 0, 1),
+    "blender": (2, 7, 8),
+    "location": "",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Qt"}
+
+import bpy
+import sys, os, logging
+
+from qt_integration import config
+if config.MODULE_PATH:
+    if os.path.exists(config.MODULE_PATH):
+        try:
+            sys.path.append(config.MODULE_PATH)
+            from PyQt5 import QtGui, QtWidgets, QtCore
+            from qt_integration import qt_window
+            from qt_integration import ui
+        except:
+            print('klkj')
+            logging.error("Can't find the PyQt5 module")
+else:
+    logging.error("No MODULE_PATH is configured in config.py")
+
+
+
+
+class QT_WINDOW_EventLoopOp(bpy.types.Operator):
+    bl_idname = "qt_window.event_loop"
+    bl_label = "PyQt Event Loop"
+    
+    _timer = None
+    
+    def modal(self, context, event):
+        wm = context.window_manager
+        if self.window.widget_close:
+            logging.debug('cancel modal operator')
+            wm.event_timer_remove(self._timer)
+            return {"CANCELLED"}
+        else:
+            logging.debug('process the events for Qt window')
+            self.event_loop.processEvents()
+            self.app.sendPostedEvents(None, 0)
+            
+        
+        return {'PASS_THROUGH'}
+
+
+    def execute(self, context):
+        logging.debug('execute operator')
+        
+        self.app = QtWidgets.QApplication.instance()
+        ''' instance() gives the possibility to have multiple windows and close it one by one '''
+        if self.app is None:
+            self.app = QtWidgets.QApplication(['blender'])
+        self.event_loop = QtCore.QEventLoop()
+        
+        self.window = qt_window.EXAMPLE_Widget()
+        self.window.context = context
+        
+        logging.debug(self.app)
+        logging.debug(self.window)
+        
+        # run modal
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(0.05, context.window)
+        context.window_manager.modal_handler_add(self)
+        
+        return {'RUNNING_MODAL'}
+    
+
+
+def register():
+    bpy.utils.register_module(__name__)
+    
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    
+if __name__ == "__main__":
+    register()
+
---
+++ addons/qt_integration/qt_window.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys, logging, os
+from PyQt5 import QtGui, QtWidgets, QtCore
+
+class EXAMPLE_Widget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.widget_close = None
+        self.context = None
+        self.initUI()
+        
+    def initUI(self):
+        self.resize(720, 300)
+        self.setWindowTitle("QT Window")
+        #self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+        
+        label = QtWidgets.QLabel('Label')
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(label)
+        self.setLayout(layout)
+        
+        self.show()
+    
+    def closeEvent(self, event):
+        self.widget_close = True
+        self.deleteLater()
+    
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyle("Plastique")
+    window = EXAMPLE_Widget()
+    sys.exit(app.exec())
+    
+
