2253f4a64eaaadcc37794f67aa8f9002df547324
Vincent Girès <vincent.gires@gmail.com>
2017-01-04 01:53:48 +0100

    change naming classes and add remove operator

--- addons/movie_assembly/addon_preferences.py
+++ addons/movie_assembly/addon_preferences.py
@@ -1,11 +1,10 @@
 import bpy
 import sys, os, logging
 
-addon_name = os.path.basename(os.path.dirname(__file__))
+ADDON_NAME = os.path.basename(os.path.dirname(__file__))
 
 
-
-class MOVIE_ASSEMBLY_Project_Settings(bpy.types.PropertyGroup):
+class MovieAssemblyProjects(bpy.types.PropertyGroup):
     
     name = bpy.props.StringProperty(
         name = 'Name',
@@ -23,34 +22,52 @@ class MOVIE_ASSEMBLY_Project_Settings(bpy.types.PropertyGroup):
     )
 
 
-class MOVIE_ASSEMBLY_Addon_Preferences(bpy.types.AddonPreferences):
-    bl_idname = addon_name
+class MovieAssemblyAddonPreferences(bpy.types.AddonPreferences):
+    bl_idname = ADDON_NAME
     
-    project_settings = bpy.props.CollectionProperty(type=MOVIE_ASSEMBLY_Project_Settings)
+    projects = bpy.props.CollectionProperty(type=MovieAssemblyProjects)
     
     def draw(self, context):
         layout = self.layout
         layout.operator('movie_assembly_add_project.btn')
         
         
-        for project in self.project_settings:
+        for index, project in enumerate(self.projects):
             box = layout.box()
             box.prop(project, 'name')
             box.prop(project, 'dirpath')
             box.prop(project, 'filename')
-        
+            remove = box.operator('movie_assembly_remove_project.btn')
+            remove.projet_index = index
 
 
 
-class MOVIE_ASSEMBLY_Add_Project(bpy.types.Operator):
+class MovieAssemblyAddProject(bpy.types.Operator):
     bl_idname = 'movie_assembly_add_project.btn'
     bl_label = 'Add new project'
+    bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
         user_preferences = context.user_preferences
-        addon_prefs = user_preferences.addons[addon_name].preferences
+        addon_prefs = user_preferences.addons[ADDON_NAME].preferences
+        addon_prefs.projects.add()
         
-        addon_prefs.project_settings.add()
-        
+        return{'FINISHED'}
+
+
+class MovieAssemblyRemoveProject(bpy.types.Operator):
+    bl_idname = 'movie_assembly_remove_project.btn'
+    bl_label = 'Remove'
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    projet_index = bpy.props.IntProperty(
+        name = 'Index',
+        default = 0
+    )
+    
+    def execute(self, context):
+        user_preferences = context.user_preferences
+        addon_prefs = user_preferences.addons[ADDON_NAME].preferences
+        addon_prefs.projects.remove(self.projet_index)
         
         return{'FINISHED'}
