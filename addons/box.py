8c3037fa14d3156ef0922c93628bf73f2e7bf789
Vincent Gires <vincent.gires@gmail.com>
2017-12-22 22:53:54 +0100

    procedural panel modeling

--- addons/box.py
+++ addons/box.py
@@ -14,6 +14,38 @@ bl_info = {
 import bpy
 
 
+def create_panel(name='Panel', width=1.0, height=1.0, thickness=1.0):
+    context = bpy.context
+    data = bpy.data
+    
+    verts = [
+        (0.0, 0.0, 0.0),
+        (width, 0.0, 0.0),
+        (0.0, 0.0, height),
+        (width, 0.0, height),
+        (0.0, thickness, 0.0),
+        (width, thickness, 0.0),
+        (0.0, thickness, height),
+        (width, thickness, height)
+        ]
+    
+    faces = [
+        (1, 0, 2, 3),
+        (7, 6, 4, 5),
+        (3, 2, 6, 7),
+        (5, 4, 0, 1),
+        (2, 0, 4, 6),
+        (7, 5, 1, 3)
+        ]
+    
+    mesh = data.meshes.new('Mesh')
+    mesh.from_pydata(verts, [], faces)
+    object = data.objects.new(name, mesh)
+    context.scene.objects.link(object)
+    
+    return object
+
+'''
 def create_cube(name='Cube'):
     context = bpy.context
     data = bpy.data
@@ -32,12 +64,14 @@ def create_cube(name='Cube'):
         x, y, z = v
         verts[i] = (x+offset, y+offset, z+offset)
     
-    faces = [(0, 1, 2, 3),
-             (4, 7, 6, 5),
-             (0, 4, 5, 1),
-             (1, 5, 6, 2),
-             (2, 6, 7, 3),
-             (4, 0, 3, 7)]
+    faces = [
+        (0, 1, 2, 3),
+        (4, 7, 6, 5),
+        (0, 4, 5, 1),
+        (1, 5, 6, 2),
+        (2, 6, 7, 3),
+        (4, 0, 3, 7)
+        ]
     
     mesh = data.meshes.new('Mesh')
     mesh.from_pydata(verts, [], faces)
@@ -45,39 +79,39 @@ def create_cube(name='Cube'):
     context.scene.objects.link(object)
     
     return object
-
+'''
 
 def create_panels(width, length, height, thickness):
     
     panels = []
     
-    bottom = create_cube('bottom')
-    bottom.scale = (width, length, thickness)
-    panels.append(bottom)
+    #bottom = create_cube('bottom')
+    #bottom.scale = (width, length, thickness)
+    #panels.append(bottom)
     
     # cover
     #
     #
     
-    side_a = create_cube('side_a')
+    side_a = create_panel('side_a', width, height, thickness)
     side_a.location = (0, 0, thickness)
-    side_a.scale = (thickness, length, height-thickness)
+    #side_a.scale = (thickness, length, height-thickness)
     panels.append(side_a)
     
-    side_b = create_cube('side_b')
-    side_b.location = (width-thickness, 0, thickness)
-    side_b.scale = (thickness, length, height-thickness)
-    panels.append(side_b)
+    #side_b = create_cube('side_b')
+    #side_b.location = (width-thickness, 0, thickness)
+    #side_b.scale = (thickness, length, height-thickness)
+    #panels.append(side_b)
     
-    front = create_cube('front')
-    front.location = (thickness, 0, thickness)
-    front.scale = (width-(thickness*2), thickness, height-thickness)
-    panels.append(front)
+    #front = create_cube('front')
+    #front.location = (thickness, 0, thickness)
+    #front.scale = (width-(thickness*2), thickness, height-thickness)
+    #panels.append(front)
     
-    back = create_cube('back')
-    back.location = (thickness, length-thickness, thickness)
-    back.scale = (width-(thickness*2), thickness, height-thickness)
-    panels.append(back)
+    #back = create_cube('back')
+    #back.location = (thickness, length-thickness, thickness)
+    #back.scale = (width-(thickness*2), thickness, height-thickness)
+    #panels.append(back)
     
     return panels
 
@@ -96,9 +130,9 @@ def create_box(name, width, length, height, thickness):
 
 class AddBox(bpy.types.Operator):
     '''Add a box/shelf/cabinet mesh.'''
-    bl_idname = "mesh.create_box"
-    bl_label = "Add box"
-    bl_description = "Create a box/shelf/cabinet mesh."
+    bl_idname = 'mesh.create_box'
+    bl_label = 'Add box'
+    bl_description = 'Create a box/shelf/cabinet mesh.'
     bl_options = {'REGISTER', 'UNDO'}
     
     width = bpy.props.FloatProperty(
@@ -147,5 +181,5 @@ def unregister():
     bpy.utils.unregister_module(__name__)
     bpy.types.INFO_MT_mesh_add.remove(menu_add_box)
  
-if __name__ == "__main__":
+if __name__ == '__main__':
     register()
