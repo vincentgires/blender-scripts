374adf65ed2c397397e98bc910977ab3e8919884
Vincent Girès <mail@vincentgires.com>
2018-01-29 20:44:32 +0100

    Update example file.

--- addons/qt_integration/example.py
+++ addons/qt_integration/example.py
@@ -1,6 +1,32 @@
 import bpy
+from PyQt5 import QtGui, QtWidgets, QtCore
 from qt_integration import QtWindowEventLoop
-from qt_integration.qt_window import ExampleWidget
+
+
+class ExampleWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.widget_close = None
+        self.context = None
+        self.initUI()
+        
+    def initUI(self):
+        self.resize(720, 300)
+        self.setWindowTitle("QT Window")
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        
+        label = QtWidgets.QLabel('Label')
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(label)
+        self.setLayout(layout)
+        
+        self.show()
+    
+    def closeEvent(self, event):
+        self.widget_close = True
+        self.deleteLater()
+
 
 class CustomWindow(QtWindowEventLoop):
     bl_idname = 'screen.custom_window'
@@ -8,6 +34,7 @@ class CustomWindow(QtWindowEventLoop):
     
     def __init__(self):
         super().__init__(widget=ExampleWidget)
+
 
 class QtPanelExample(bpy.types.Panel):
     bl_label = 'Qt'
@@ -19,3 +46,9 @@ class QtPanelExample(bpy.types.Panel):
         scene = context.scene
         layout = self.layout
         layout.operator('screen.custom_window')
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExempleWidget()
+    sys.exit(app.exec())
