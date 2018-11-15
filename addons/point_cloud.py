d03100eaee256da8d5d112a698d2ab5ca4d74edc
Vincent Girès <mail@vincentgires.com>
2018-11-15 17:50:04 +0100

    PEP8

--- addons/point_cloud.py
+++ addons/point_cloud.py
@@ -16,6 +16,8 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+import bpy
+import bgl
 
 bl_info = {
     'name': 'Point Cloud',
@@ -28,92 +30,80 @@ bl_info = {
     'tracker_url': '',
     'category': '3D View'}
 
-
-import bpy
-import bgl
-
 cloud_coordinates = []
 
-## CUSTOM PROPERTIES ##
-#######################
 
 class PointCloudProperties(bpy.types.PropertyGroup):
     color_pass = bpy.props.StringProperty(
         name='Color pass')
-    
     position_pass = bpy.props.StringProperty(
         name='Position pass')
-    
     point_detail = bpy.props.FloatProperty(
         name='Detail',
         default=0.25,
         min=0.001,
         max=1.0)
-    
     point_size = bpy.props.IntProperty(
         name='Size',
         default=1,
         min=1)
 
 
-## FUNCTIONS ##
-###############
-
 def draw_pointcloud_gl():
     context = bpy.context
     scene = context.scene
-    
+
     if not cloud_coordinates:
         return None
-    
+
     detail = scene.point_cloud.point_detail
     length_full = len(cloud_coordinates)
     length_detail = length_full*detail
     step = length_full/length_detail
     step = int(step)
     size = scene.point_cloud.point_size
-    
+
     bgl.glPointSize(size)
     bgl.glBegin(bgl.GL_POINTS)
-    
+
     for coord in cloud_coordinates[::step]:
         position, color = coord
         r, g, b = color
         x, y, z = position
         bgl.glColor3f(r, g, b)
         bgl.glVertex3f(x, y, z)
-    
+
     bgl.glEnd()
 
 
 def get_positions(context):
     scene = context.scene
     data = bpy.data
-    
+
     position_src = scene.point_cloud.position_pass
     position_data = data.images[position_src]
     position_pixels = list(position_data.pixels)
-    
+
     coordinates = []
     cpt_rgba = 0
     pixel_rgb = []
-    
+
     context.window.cursor_set('WAIT')
-    
+
     for value in position_pixels:
-        
+
         if cpt_rgba <= 2:
             pixel_rgb.append(value)
-            
+
         cpt_rgba += 1
-        
+
         if cpt_rgba == 3:
             coordinates.append(pixel_rgb)
             pixel_rgb = []
-        
+
         elif cpt_rgba == 4:
             cpt_rgba = 0
-    
+
     context.window.cursor_set('DEFAULT')
     return coordinates
 
@@ -121,37 +111,38 @@ def get_positions(context):
 def get_coordinates(context):
     scene = context.scene
     data = bpy.data
-    
+
     position_src = scene.point_cloud.position_pass
     color_src = scene.point_cloud.color_pass
     position_data = data.images[position_src]
     color_data = data.images[color_src]
     position_pixels = list(position_data.pixels)
     color_pixels = list(color_data.pixels)
-    
+
     cpt_rgba = 0
     pixel_rgb_position = []
     pixel_rgb_color = []
-    
+
     context.window.cursor_set('WAIT')
-    
+
     for value_position, value_color in zip(position_pixels, color_pixels):
-        
+
         if cpt_rgba <= 2:
             pixel_rgb_position.append(value_position)
             pixel_rgb_color.append(value_color)
-            
+
         cpt_rgba += 1
-        
+
         if cpt_rgba == 3:
             cloud_coordinates.append((pixel_rgb_position, pixel_rgb_color))
             pixel_rgb_position = []
             pixel_rgb_color = []
-            
+
         elif cpt_rgba == 4:
             cpt_rgba = 0
-    
+
     context.window.cursor_set('DEFAULT')
+
 
 def create_mesh(name, origin, verts, edges, faces):
     me = bpy.data.meshes.new(name+'Mesh')
@@ -161,30 +152,29 @@ def create_mesh(name, origin, verts, edges, faces):
     bpy.context.scene.objects.link(ob)
     me.from_pydata(verts, edges, faces)
     me.update(calc_edges=True)
-    
+
     return ob
 
-
-## PANEL ##
-###########
 
 class PointCloud3DViewPanel(bpy.types.Panel):
     bl_label = 'Point Cloud'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_category = 'Custom'
- 
+
     def draw(self, context):
         scene = context.scene
         data = bpy.data
         layout = self.layout
-        
+
         col = layout.column(align=True)
-        col.prop_search(scene.point_cloud, 'position_pass',
-                        data, 'images', icon='FILE_IMAGE')
-        col.prop_search(scene.point_cloud, 'color_pass',
-                        data, 'images', icon='FILE_IMAGE')
-        
+        col.prop_search(
+            scene.point_cloud, 'position_pass',
+            data, 'images', icon='FILE_IMAGE')
+        col.prop_search(
+            scene.point_cloud, 'color_pass',
+            data, 'images', icon='FILE_IMAGE')
+
         col = layout.column(align=True)
         row = col.row(align=True)
         text = 'Update cloud' if cloud_coordinates else 'Generate cloud'
@@ -195,20 +185,17 @@ class PointCloud3DViewPanel(bpy.types.Panel):
         col.operator('scene.pointcloud_generate_mesh')
 
 
-## OPERATOR ##
-##############
-
 class PointCloudGenerateOpenGl(bpy.types.Operator):
     bl_idname = 'scene.pointcloud_generate'
     bl_label = 'Generate cloud'
     bl_description = 'Generate cloud of point based on the position pass'
-    
+
     @classmethod
     def poll(cls, context):
         position = context.scene.point_cloud.position_pass
         color = context.scene.point_cloud.color_pass
         return position and color
-    
+
     def execute(self, context):
         scene = context.scene
         coordinates = get_coordinates(context)
@@ -219,11 +206,11 @@ class PointCloudClearOpenGl(bpy.types.Operator):
     bl_idname = 'scene.pointcloud_clear'
     bl_label = 'Clear'
     bl_description = 'Remove cloud from the viewport'
-    
+
     @classmethod
     def poll(cls, context):
         return cloud_coordinates
-    
+
     def execute(self, context):
         cloud_coordinates.clear()
         return{'FINISHED'}
@@ -233,23 +220,23 @@ class PointCloudGenerateMesh(bpy.types.Operator):
     bl_idname = 'scene.pointcloud_generate_mesh'
     bl_label = 'Create mesh'
     bl_description = 'Generate cloud of vertices based on the position pass'
-    
+
     @classmethod
     def poll(cls, context):
         position = context.scene.point_cloud.position_pass
         return position
-    
+
     def execute(self, context):
         scene = context.scene
-        
         coordinates = get_positions(context)
-        origin = (0,0,0)
+        origin = (0, 0, 0)
         position_objects = create_mesh(
             'Position_Cloud', origin, coordinates, [], [])
-        
         return{'FINISHED'}
 
+
 opengl_handle = [None]
+
 
 def register():
     bpy.utils.register_module(__name__)
@@ -257,15 +244,14 @@ def register():
         bpy.props.PointerProperty(type=PointCloudProperties)
     opengl_handle[0] = bpy.types.SpaceView3D.draw_handler_add(
         draw_pointcloud_gl, (), 'WINDOW', 'POST_VIEW')
-    
+
+
 def unregister():
     bpy.utils.unregister_module(__name__)
     del bpy.types.Scene.point_cloud
     bpy.types.SpaceView3D.draw_handler_remove(
         opengl_handle[0], 'WINDOW')
 
-    
+
 if __name__ == '__main__':
     register()
-
-
