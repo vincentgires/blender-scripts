ef8b9d03bffb66a850d9bc197c427e09481be898
Vincent Girès <mail@vincentgires.com>
2019-11-26 23:29:55 +0100

    Fix spelling mistake

--- scripts/modules/vgblender/image.py
+++ scripts/modules/vgblender/image.py
@@ -3,7 +3,7 @@ from bpy import data
 
 def get_image_resolution(image):
     if image.type == 'MULTILAYER':
-        # HACK to get the resolution of a multilayer EXR though movieclip
+        # HACK to get the resolution of a multilayer EXR through movieclip
         movieclip = data.movieclips.load(image.filepath)
         x, y = movieclip.size
         data.movieclips.remove(movieclip)
