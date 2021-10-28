94031a2e73430f9810c6f8ad865e14998c82dba2
Vincent Girès <mail@vincentgires.com>
2021-10-29 01:43:27 +0200

    Fix crash when removing the node

--- scripts/addons/data_nodes/nodes/distance.py
+++ scripts/addons/data_nodes/nodes/distance.py
@@ -1,11 +1,10 @@
 # TODO BUG:
 # - draw handler does not load on existing node opened in a blend file
-# - crash when removing the node
 
 import bpy
 import bgl
 import math
-from bpy.types import Node
+from bpy.types import Node, SpaceView3D
 from ..utils import send_value
 import gpu
 from gpu_extras.batch import batch_for_shader
@@ -13,6 +12,7 @@ from gpu_extras.batch import batch_for_shader
 shader_3d_uniform = (
     gpu.shader.from_builtin('3D_UNIFORM_COLOR')
     if not bpy.app.background else None)
+draw_handler = {}
 
 
 def draw_line(v1, v2, color, width=2):
@@ -35,6 +35,12 @@ def draw_distance_opengl(self, context):
     draw_line(a, b, (1.0, 1.0, 1.0))
 
 
+def _force_redraw_view3d():
+    for area in bpy.context.screen.areas:
+        if area.type in 'VIEW_3D':
+            area.tag_redraw()
+
+
 class DistanceNode(Node):
     """Distance node"""
     bl_idname = 'DistanceNodeType'
@@ -44,24 +50,24 @@ class DistanceNode(Node):
         self.inputs.new('NodeSocketVector', 'VectorA')
         self.inputs.new('NodeSocketVector', 'VectorB')
         self.outputs.new('NodeSocketFloat', 'Distance')
-        self.draw_handler = bpy.types.SpaceView3D.draw_handler_add(
+        # Use memory address to get unique key used in draw handler
+        draw_handler[self.as_pointer()] = SpaceView3D.draw_handler_add(
             draw_distance_opengl, (self, context), 'WINDOW', 'POST_VIEW')
 
     def update(self):
+        print('update', self.as_pointer())
         if len(self.inputs) >= 2:
             a = self.inputs['VectorA'].default_value
             b = self.inputs['VectorB'].default_value
             distance = math.sqrt(
                 (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2 + (b[2] - a[2]) ** 2)
             send_value(self.outputs, distance)
-
-            # Force redraw 3d view
-            for area in bpy.context.screen.areas:
-                if area.type in 'VIEW_3D':
-                    area.tag_redraw()
+            _force_redraw_view3d()
 
     def free(self):
-        bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
+        SpaceView3D.draw_handler_remove(
+            draw_handler[self.as_pointer()], 'WINDOW')
+        _force_redraw_view3d()
 
     def draw_label(self):
         return 'Distance'
