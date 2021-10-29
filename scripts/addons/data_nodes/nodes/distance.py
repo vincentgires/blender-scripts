f77227c85939a03e161859ced215bd8799e30f1c
Vincent Girès <mail@vincentgires.com>
2021-10-29 10:34:48 +0200

    Fix draw_handler that wasn't always set

--- scripts/addons/data_nodes/nodes/distance.py
+++ scripts/addons/data_nodes/nodes/distance.py
@@ -1,6 +1,3 @@
-# TODO BUG:
-# - draw handler does not load on existing node opened in a blend file
-
 import bpy
 import bgl
 import math
@@ -29,9 +26,9 @@ def draw_line(v1, v2, color, width=2):
     bgl.glDisable(bgl.GL_BLEND)
 
 
-def draw_distance_opengl(self, context):
-    a = tuple(self.inputs['VectorA'].default_value)
-    b = tuple(self.inputs['VectorB'].default_value)
+def draw_distance_opengl(node, context):
+    a = tuple(node.inputs['VectorA'].default_value)
+    b = tuple(node.inputs['VectorB'].default_value)
     draw_line(a, b, (1.0, 1.0, 1.0))
 
 
@@ -46,16 +43,21 @@ class DistanceNode(Node):
     bl_idname = 'DistanceNodeType'
     bl_label = 'Distance'
 
+    def _set_draw_handler(self):
+        # Use memory address to get unique key used in draw handler
+        if self.as_pointer() in draw_handler:
+            return
+        draw_handler[self.as_pointer()] = SpaceView3D.draw_handler_add(
+            draw_distance_opengl, (self, bpy.context), 'WINDOW', 'POST_VIEW')
+
     def init(self, context):
         self.inputs.new('NodeSocketVector', 'VectorA')
         self.inputs.new('NodeSocketVector', 'VectorB')
         self.outputs.new('NodeSocketFloat', 'Distance')
-        # Use memory address to get unique key used in draw handler
-        draw_handler[self.as_pointer()] = SpaceView3D.draw_handler_add(
-            draw_distance_opengl, (self, context), 'WINDOW', 'POST_VIEW')
+        self._set_draw_handler()
 
     def update(self):
-        print('update', self.as_pointer())
+        self._set_draw_handler()
         if len(self.inputs) >= 2:
             a = self.inputs['VectorA'].default_value
             b = self.inputs['VectorB'].default_value
@@ -63,6 +65,9 @@ class DistanceNode(Node):
                 (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2 + (b[2] - a[2]) ** 2)
             send_value(self.outputs, distance)
             _force_redraw_view3d()
+
+    def copy(self, node):
+        self._set_draw_handler()
 
     def free(self):
         SpaceView3D.draw_handler_remove(
