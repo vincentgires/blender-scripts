1fc63e1b5abc8258066efce78b1e0bc62ddf63dd
Vincent Girès <vincent@vincent.bio>
2022-05-12 01:33:08 +0200

    Add import objects from alembic file function

---
+++ scripts/modules/vgblender/alembic.py
@@ -0,0 +1,20 @@
+import bpy
+
+
+def load_objects_from_alembic(path, filter_type=None):
+    context = bpy.context
+    data = bpy.data
+    current_scene = context.scene
+
+    temporary_scene = data.scenes.new(name='tmp scene')
+    context.window.scene = temporary_scene
+    bpy.ops.wm.alembic_import(filepath=path)
+    if filter_type is None:
+        objects = [ob for ob in temporary_scene.objects]
+    else:
+        objects = [ob for ob in temporary_scene.objects
+                   if ob.type == filter_type]
+
+    context.window.scene = current_scene
+    data.scenes.remove(temporary_scene)
+    return objects
