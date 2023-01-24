a0cae1838719e50aa8c4907dd6f90c7ae5d3ed12
Vincent Girès <vincent@vincent.bio>
2023-01-24 13:59:34 +0100

    Add conform data paths function

--- scripts/modules/vgblender/path.py
+++ scripts/modules/vgblender/path.py
@@ -1,18 +1,21 @@
 import os
 import sys
+import bpy
 
 
 def normpath(path):
-    # remove double slash to be able to use it in Blender
-    # result: /my/path
+    """Set path to correct OS and Blender convention"""
+
+    # Remove double slash to be able to use it in Blender
     if sys.platform.startswith('linux'):
         path = path.replace('//', '/')
+    # Result: /my/path
 
-    # make linux path compatible with windows
-    # result after normpath: \\my\path
+    # Make linux path compatible with windows
     elif sys.platform.startswith('win'):
         if path.startswith('/') and not path.startswith('//'):
             path = '/{}'.format(path)
+    # Result after normpath: \\my\path
 
     path = os.path.normpath(path)
     return path
@@ -26,3 +29,21 @@ def convert_os_path(path):
         if path.startswith('/') and not path.startswith('//'):
             path = r'\\' + path[1:]
     return path
+
+
+def conform_data_paths():
+    """Adapt paths for Linux or Windows"""
+
+    for scene in bpy.data.scenes:
+        scene.render.filepath = convert_os_path(scene.render.filepath)
+        if not scene.sequence_editor:
+            continue
+        sequences = scene.sequence_editor.sequences_all
+        for strip in sequences:
+            match strip.type:
+                case 'MOVIE':
+                    strip.filepath = convert_os_path(strip.filepath)
+                case 'IMAGE':
+                    strip.directory = convert_os_path(strip.directory)
+    for image in bpy.data.images:
+        image.filepath = convert_os_path(image.filepath)
