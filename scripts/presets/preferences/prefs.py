fa1fe2e22c94cc3efde90e964ec42e701aaf4db9
Vincent Girès <vincent@vincent.bio>
2023-02-20 01:48:48 +0100

    Add preferences preset

---
+++ scripts/presets/preferences/prefs.py
@@ -0,0 +1,36 @@
+import bpy
+from operator import attrgetter
+
+assignation = {
+    'use_preferences_save': False,
+    'view': {
+        'show_splash': False,
+        'show_developer_ui': True,
+        'show_tooltips_python': True
+    },
+    'inputs': {
+        'use_emulate_numpad': True,
+        'use_mouse_continuous': True,
+        'use_drag_immediately': False
+    },
+    'system': {
+        'memory_cache_limit': 12288,
+        'sequencer_proxy_setup': 'MANUAL'
+    },
+    'filepaths': {
+        'save_version': 0
+    }
+}
+
+preferences = bpy.context.preferences
+
+for k, v in assignation.items():
+    if not isinstance(v, dict):
+        setattr(preferences, k, v)
+    else:
+        for attr_name, attr_value in v.items():
+            obj = attrgetter(k)(preferences)
+            setattr(obj, attr_name, attr_value)
+
+wm = bpy.context.window_manager
+wm.keyconfigs['Blender'].preferences.select_mouse = 'RIGHT'
