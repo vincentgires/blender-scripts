26a6dbfc3a164c9d34b8d7aeda50a8b64aa15283
Vincent Girès <vincent@vincent.bio>
2023-05-15 01:57:37 +0200

    Use lowercase

--- scripts/modules/vgblender/render.py
+++ scripts/modules/vgblender/render.py
@@ -13,14 +13,14 @@ def render_movie(scene, codec=None, qscale=None, metadata=None):
     """
     codec = codec or 'mjpeg'
 
-    TEMPORARY_FORMAT = 'JPEG'
-    TEMPORARY_EXT = '.jpg'
+    tmp_format = 'JPEG'
+    tmp_ext = '.jpg'
 
     output = scene.render.filepath
     render_tmp = tempfile.mkdtemp()
     scene.render.filepath = os.path.join(
-        render_tmp, 'render.####' + TEMPORARY_EXT)
-    scene.render.image_settings.file_format = TEMPORARY_FORMAT
+        render_tmp, 'render.####' + tmp_ext)
+    scene.render.image_settings.file_format = tmp_format
     scene.render.image_settings.quality = 100
 
     bpy.ops.render.render(animation=True)
@@ -30,7 +30,7 @@ def render_movie(scene, codec=None, qscale=None, metadata=None):
         'ffmpeg',
         '-framerate', str(scene.render.fps),
         '-start_number', str(scene.frame_start),
-        '-i', '{}/render.%04d'.format(render_tmp) + TEMPORARY_EXT,
+        '-i', '{}/render.%04d'.format(render_tmp) + tmp_ext,
         '-c:v', codec]
     if qscale is not None:
         command.extend(['-q:v', qscale])
